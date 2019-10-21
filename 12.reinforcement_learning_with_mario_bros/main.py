a56f3c9718ec9f2c0e4bba7fc5abed1a663d7a4f
yingshaoxo <yingshaoxo@users.noreply.github.com>
2019-10-21 19:20:40 +0800

    Update main.py

--- 12.reinforcement_learning_with_mario_bros/main.py
+++ 12.reinforcement_learning_with_mario_bros/main.py
@@ -1,8 +1,8 @@
-from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
+from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 env = gym_super_mario_bros.make('SuperMarioBros-v1')
-env = BinarySpaceToDiscreteSpaceEnv(env, SIMPLE_MOVEMENT)
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
 from random import randint
 import numpy as np
