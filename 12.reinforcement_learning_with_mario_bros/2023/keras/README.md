0ff4d3b861f55a12956257cd5ebb60ee10c1d432
yingshaoxo <yingshaoxo@gmail.com>
2023-05-12 23:15:31 +0800

    fix graph error

--- 12.reinforcement_learning_with_mario_bros/2023/keras/README.md
+++ 12.reinforcement_learning_with_mario_bros/2023/keras/README.md
@@ -8,3 +8,8 @@ sudo apt install pip -y
 
 python3.10 -m pip install -r requirements.txt
 ```
+
+## Theory
+1. actor_model takes (state), returns (action_probability_list); reward_model takes (state, action), returns (reward)
+<!-- 2. why reward_model could make the actor_model perform better over time??? -->
+2. we train the actor_model by giving it x=(state), y=(the optimal action predicted by reward_model)
--- 12.reinforcement_learning_with_mario_bros/2023/keras/fetch_training_data.py
+++ 12.reinforcement_learning_with_mario_bros/2023/keras/fetch_training_data.py
@@ -7,6 +7,8 @@ from random import randint
 import os
 import cv2
 
+from pymongo import MongoClient
+
 
 class Trainer:
     def __init__(self):
@@ -29,6 +31,16 @@ class Trainer:
         self.model_file_path = './nn_keras_model'
         self.model = self.generate_model()
 
+        self.mongo_client = MongoClient('mongodb://localhost:27017/')
+        self.training_data_collection = self.mongo_client['mario']['training_data_collection']
+    
+    def get_all_training_data(self):
+        for one in self.training_data_collection.find():
+            print(one)
+    
+    def add_training_data(self, one):
+        self.training_data_collection.insert_one(one)
+
     def generate_model(self):
         action_image_input = keras.Input(shape=(self.img_rows, self.img_cols, 1), name="action_image_input")
         x = keras.layers.Conv2D(32, 8, strides=(4,4), activation='relu')(action_image_input)
@@ -38,13 +50,25 @@ class Trainer:
         x = keras.layers.Flatten()(x)
         commom_layer = keras.layers.Dense(512, activation='relu')(x)
 
+        # last_action_input = keras.Input(shape=(1), name="last_action_input")
+        # x2 = keras.layers.Dense(self.number_of_actions, activation='relu')(last_action_input)
+        # x2 = keras.layers.concatenate([commom_layer, x2])
+        # x2 = keras.layers.Dense(128, activation='relu')(x2)
+        # action_output = keras.layers.Dense(self.number_of_actions, activation="softmax", name="action_output")(x2)
+
         x2 = keras.layers.Dense(128, activation='relu')(commom_layer)
         x2 = keras.layers.Dense(128, activation='relu')(x2)
+        x2 = keras.layers.Dense(64, activation='relu')(x2)
+        x2 = keras.layers.Dense(32, activation='relu')(x2)
         action_output = keras.layers.Dense(self.number_of_actions, activation="softmax", name="action_output")(x2)
 
-        reward_output = keras.layers.Dense(1, activation="linear", name="reward_output")(commom_layer)
+        action_input = keras.Input(shape=(1), name="action_input")
+        x3 = keras.layers.Dense(self.number_of_actions, activation='relu')(action_input)
+        x3 = keras.layers.concatenate([commom_layer, x3])
+        x3 = keras.layers.Dense(64, activation='relu')(x3)
+        reward_output = keras.layers.Dense(1, activation='linear', name="reward_output")(x3)
 
-        model = keras.Model([action_image_input], [action_output, reward_output], name="yingshaoxo_and_mario")
+        model = keras.Model([action_image_input, action_input], [action_output, reward_output], name="yingshaoxo_and_mario")
         #print(model.summary())
 
         model.compile(
@@ -54,7 +78,7 @@ class Trainer:
                 "reward_output": "huber",
             },
             loss_weights={
-                "action_output": 0.5,
+                "action_output": 0.8,
                 "reward_output": 1,
             },
             metrics=["accuracy"],
@@ -95,7 +119,7 @@ class Trainer:
         # info = {'coins': 0, 'flag_get': False, 'life': 3, 'score': 0, 'stage': 1, 'status': 'small', 'time': 400, 'world': 1, 'x_pos': 40}
 
         gamma = 0.99  # Discount factor for past rewards
-        max_steps_per_episode = 3000
+        max_steps_per_episode = 100
         eps = np.finfo(np.float32).eps.item()  # Smallest number such that 1.0 + eps != 1.0
 
         done = True
@@ -109,42 +133,91 @@ class Trainer:
         running_reward = 0
         episode_count = 0
 
+        time_jump = 50
+        same_action_count = 0
+        is_in_time_jump = False
+
         while 1:
             state, _ = self.env.reset()
             state = self.image_process(state)
+            action = 0
             episode_reward = 0
 
-            for step in range(max_steps_per_episode):
+            temp_reward = 0
+
+            step_counting = 0
+            while True:
+                if step_counting == max_steps_per_episode and is_in_time_jump == False:
+                    break
+
                 if done:
                     state, _ = self.env.reset()
                     state = self.image_process(state)
+                    action = 0
 
-                state_history.append(state)
+                if is_in_time_jump == False:
+                    is_in_time_jump = True
+                    same_action_count = 0
 
-                predict_action_probabilitys, predict_reward = self.model.predict(
-                    {
-                        "action_image_input": np.expand_dims(state, axis=0), 
-                    }
-                )
-                predict_rewards_history.append(predict_reward[0][0])
-                print(predict_reward[0][0])
+                    step_counting += 1
 
-                action = np.random.choice(self.number_of_actions, p=np.squeeze(predict_action_probabilitys[0]))
-                predict_action_history.append(action)
-                predict_action_probability_history.append(tf.math.log(predict_action_probabilitys[0][action]))
-                last_predict_action_probability_distribution = predict_action_probabilitys[0]
+                    state_history.append(state)
 
-                result = self.env.step(action)
+                    a_temp_list_for_reward_and_action = []
+                    action_to_reward_dict = {}
+                    for each_action in range(self.number_of_actions):
+                        predict_action_probabilitys, predict_reward = self.model.predict(
+                            {
+                                "action_image_input": np.expand_dims(state, axis=0), 
+                                "action_input": np.expand_dims(each_action, axis=0)
+                            }
+                        )
+                        a_temp_list_for_reward_and_action.append((predict_reward, each_action))
+                        action_to_reward_dict[each_action] = predict_reward
 
-                state, reward, terminated1, terminated2, info = result
-                done = terminated1 or terminated2
-                state = self.image_process(state)
+                    a_temp_list_for_reward_and_action.sort(key=lambda item: item[0])
+                    action = a_temp_list_for_reward_and_action[-1][1]
 
-                real_rewards_history.append(reward)
-                episode_reward += reward
+                    # if predict_reward > 0:
+                    predict_action_history.append(action)
+                    predict_rewards_history.append(action_to_reward_dict[action])
+                    last_predict_action_probability_distribution = a_temp_list_for_reward_and_action.copy()
+                    # else:
+                    #     action = np.random.choice(self.number_of_actions, p=np.squeeze(predict_action_probabilitys[0]))
+                    #     predict_action_history.append(action)
+                    #     predict_rewards_history.append(action_to_reward_dict[action])
+                    #     # predict_action_probability_history.append(tf.math.log(predict_action_probabilitys[0][action]))
+                    #     last_predict_action_probability_distribution = predict_action_probabilitys[0]
+                else:
+                    same_action_count += 1
+                    if same_action_count == time_jump:
+                        is_in_time_jump = False
 
-                self.env.render()
+                        real_rewards_history.append(temp_reward)
+                        episode_reward += temp_reward
+                        temp_reward = 0
 
+                        continue
+
+                    result = self.env.step(action)
+                    state, reward, terminated1, terminated2, info = result
+                    state = self.image_process(state)
+                    done = terminated1 or terminated2
+                    temp_reward += reward
+                    if done == True:
+                        state, _ = self.env.reset()
+                        state = self.image_process(state)
+                        action = 0
+
+                        is_in_time_jump = False
+
+                        temp_reward -= 100
+                        real_rewards_history.append(temp_reward)
+                        episode_reward += temp_reward
+                        temp_reward = 0
+
+                    self.env.render()
+            
             running_reward = 0.05 * episode_reward + (1 - 0.05) * running_reward
             continues_real_rewards = []
             discounted_sum = 0
@@ -159,6 +232,7 @@ class Trainer:
             self.model.fit(
                 x= {
                     "action_image_input": np.array(state_history), 
+                    "action_input": np.array(predict_action_history)
                 }, 
                 y={
                     "action_output": np.array(predict_action_history),
@@ -184,5 +258,6 @@ class Trainer:
 
 if __name__ == "__main__":
     trainer = Trainer()
-    trainer.load_model()
-    trainer.run()
+    trainer.get_all_training_data()
+    # trainer.load_model()
+    # trainer.run()
