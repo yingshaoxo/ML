97170f726fc4076dd7685126ab1c146d29860f84
yingshaoxo <yingshaoxo@gmail.com>
2023-06-26 11:28:05 +0800

    update the text to speech code

--- 17.text_to_speech/main.py
+++ 17.text_to_speech/main.py
@@ -6,6 +6,8 @@
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
+import re
+
 from TTS.api import TTS
 from pprint import pprint
 
@@ -15,18 +17,29 @@ from pydub.playback import play
 from auto_everything.terminal import Terminal
 terminal = Terminal()
 
+import torch
+use_gpu = True if torch.cuda.is_available() else False
+
 pprint(TTS.list_models())
 
-tts = TTS("tts_models/en/ljspeech/fast_pitch")
+tts_en = TTS("tts_models/en/ljspeech/tacotron2-DDC", gpu=use_gpu)
+#tts_en = TTS("tts_models/en/ljspeech/fast_pitch", gpu=use_gpu)
+tts_cn = TTS("tts_models/zh-CN/baker/tacotron2-DDC-GST", gpu=use_gpu)
 
-print(tts.speakers)
-print(tts.languages)
 
-output_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "output.wav"))
+def speak_it(language: str, text: str):
+    output_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "output.wav"))
 
-text = "Hello, yingshaoxo! This is a test!"
-while True:
-    tts.tts_to_file(text=text, file_path=output_file)
+    if (language == "en"):
+        tts = tts_en
+    else:
+        tts = tts_cn
+
+    try:
+        tts.tts_to_file(text=text, file_path=output_file)
+    except Exception as e:
+        print(e)
+        tts.tts_to_file(text=text, file_path=output_file, speaker=tts.speakers[0], language=tts.languages[0], speed=2.5)
 
     # terminal.run(f"""
      #vlc -I dummy "{output_file}" "vlc://quit"
@@ -43,5 +56,61 @@ while True:
     # audio = AudioSegment.from_file(output_file)
     # audio.frame_width = 24000
     # play(audio)
+
+
+def language_splitor(text: str):
+    language_list = []
+    index = 0
+    while True:
+        temp_string = ""
+        if (index >= len(text)):
+            break
+        char = text[index]
+        while ord(char) < 128:
+            # english
+            char = text[index]
+            temp_string += char
+            index += 1
+            if (index >= len(text)):
+                break
+        if (temp_string.strip() != ""):
+            temp_string = temp_string[:-1]
+            index -= 1
+            language_list.append({
+                "language": "en",
+                "text": temp_string
+            })
+
+        temp_string = ""
+        if (index >= len(text)):
+            break
+        char = text[index]
+        while not ord(char) < 128:
+            # chinese 
+            char = text[index]
+            temp_string += char
+            index += 1
+            if (index >= len(text)):
+                break
+        if (temp_string.strip() != ""):
+            temp_string = temp_string[:-1]
+            index -= 1
+            language_list.append({
+                "language": "cn",
+                "text": temp_string
+            })
+
+        if (index+1 >= len(text)):
+            break
+
+    return language_list
+
+
+text = "Hello, yingshaoxo! I love you so much!"
+while True:
+    data_ = language_splitor(text)
+    for one in data_:
+        print(one)
+        speak_it(language=one["language"], text=one["text"])
 
     text = input("\n\nWhat you want to say?\n")
