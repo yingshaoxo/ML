c062bfad09300dff03fe6a1ce638d0c348d78183
yingshaoxo <yingshaoxo@gmail.com>
2023-07-07 01:11:50 +0800

    update text to speech code

--- 17.text_to_speech/for_linux_system/main.py
+++ 17.text_to_speech/for_linux_system/main.py
@@ -3,7 +3,7 @@
 import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')  # getting details of current voice
-engine.setProperty('voice', voice[1].id)  # this is female voice
+engine.setProperty('voice', voices[1].id)  # this is female voice
 engine.say("Hi you! my name is lili, I like yingshaoxo.")
 engine.runAndWait()
 
