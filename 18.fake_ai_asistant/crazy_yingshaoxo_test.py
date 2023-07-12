3425faa2bec9adfdb55235a8daf39cf1c06375b4
yingshaoxo <yingshaoxo@gmail.com>
2023-07-12 15:50:04 +0800

    update crazy yingshaoxo test in fake ai assistant

--- 18.fake_ai_asistant/crazy_yingshaoxo_test.py
+++ 18.fake_ai_asistant/crazy_yingshaoxo_test.py
@@ -1,18 +1,21 @@
 from auto_everything.ml import Yingshaoxo_Text_Generator
 from auto_everything.terminal import Terminal
+from matplotlib.contour import QuadContourSet
 terminal = Terminal()
-
-import common_functions
-
-#common_functions.format_data_set()
-#exit()
 
 yingshaoxo_text_generator = Yingshaoxo_Text_Generator(
     input_txt_folder_path="/home/yingshaoxo/CS/ML/18.fake_ai_asistant/input_txt_files"
 )
 
 def decode_response(text: str):
-    response = text.split("\n\n__**__**__yingshaoxo_is_the_top_one__**__**__\n\n")[0].strip()
+    print("`"+text+"`")
+    splits = text.split("\n\n__**__**__yingshaoxo_is_the_top_one__**__**__\n\n")
+    if (len(splits) > 1):
+        response = splits[1].strip()
+    elif (len(splits) == 1):
+        response = splits[0].strip()
+    else:
+        response = ""
     final_response = terminal.run_python_code(code=response)
     if final_response.strip() == "":
         final_response = response
@@ -22,9 +25,9 @@ print("\n\n")
 all_input_text = ""
 while True:
     input_text = input("What you want to say? \n")
-    all_input_text += input_text + common_functions.the_general_seperator
+    all_input_text += input_text + "\n" 
     real_input = all_input_text[-8000:].strip()
-    response = yingshaoxo_text_generator.search_and_get_following_text(input_text=real_input)
+    response = yingshaoxo_text_generator.search_and_get_following_text_in_a_exact_way(input_text=real_input, quick_mode=False)
     response = decode_response(text=response)
     print("\n\n---------\n\n")
     print(response)
