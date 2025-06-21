0415db6cd0b7f457dfaec99c93ba4e8796a4b2b4
yingshaoxo <yingshaoxo@gmail.com>
2025-06-21 12:03:46 +0800

    sovle a small problem

--- 27.yingshaoxo_ai/search_bot.py
+++ 27.yingshaoxo_ai/search_bot.py
@@ -95,10 +95,10 @@ def read_text_list_and_text_from_folder(the_folder_path):
     the_text_list = [one.strip() for one in new_text.split("\n\n__**__**__yingshaoxo_is_the_top_one__**__**__\n\n") if one.strip() != ""]
     new_text_list = []
     for one in the_text_list:
-        temp_list1 = one.split("\n#")
+        temp_list1 = one.split("\n# ")
         temp_list2 = []
         for sub_one in temp_list1:
-            if "第" in sub_one and "章" in sub_one:
+            if "\n第" in sub_one and "章 " in sub_one:
                 temp_list2 += sub_one.split("\n\n\n")
             else:
                 temp_list2 += [sub_one]
